90f2e9a20c7f64f9c9c893f4431c21367a579a60
gupta-neha12 <65106324+gupta-neha12@users.noreply.github.com>
2020-05-10 12:22:20 +0530

    Committed existing code

---
+++ controllers/message_controllers.js
@@ -0,0 +1,42 @@
+const Message = require('../models/message');
+const ObjectId = require('mongoose').Types.ObjectId;
+
+module.exports = {
+  index: (req, res) => {
+    const { conversationId } = req.query;
+    Message.find({ conversation: new ObjectId(conversationId) })
+      .then((prevMessages) => {
+        res.send(prevMessages);
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+    },
+    newMessage: (req, res) => {
+      const { senderId, conversationId, message } = req.body;
+      const newMessage = new Message();
+      newMessage.author = senderId;
+      newMessage.conversation = conversationId;
+      newMessage.message = message;
+      newMessage.isFile = false;
+      newMessage.save()
+        .then((newMessage) => {
+          res.send(newMessage);
+        });
+  },
+  uploadImage: (req, res) => {
+    const { file } = req;
+    const { originalname } = file;
+    const newMessage = new Message();
+    const indexOfHash = originalname.indexOf('-');
+    newMessage.author = originalname.substring(0, indexOfHash);
+    newMessage.conversation = originalname.substring(indexOfHash+1, (originalname.length-4));
+    newMessage.message = req.file.filename;
+    newMessage.isFile = true;
+    console.log('----newMessage-----', newMessage);
+    newMessage.save()
+      .then((newMessage) => {
+        res.send(newMessage);
+      });
+  }
+}
